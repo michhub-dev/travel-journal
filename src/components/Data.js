5090ccc683134fbc5f2c641923a4a69922dd11e8
Michelle David <rachaelholy@gmail.com>
2022-08-16 17:41:08 +0100

    added props

--- src/components/Data.js
+++ src/components/Data.js
@@ -10,7 +10,9 @@ export default function Data(props) {
                 <img src={props.logo} alt="map" />
                 <span className="location">{props.location}</span>
                 <a href={props.googleMapsUrl} className="a"/>
-                <h2></h2>
+                <h2>{props.googleMapsUrl}</h2>
+                <p>{props.startDate}-{props.endDate}</p>
+                <p>{props.description}</p>
             </div>
           </section>
         </>
